479f5585f12392c3bf81cb2f38813ddded268396
alanz <alanzgit@outlook.com>
2025-08-10 18:44:19 +0800

    init: 设置及规则

---
+++ config.py
@@ -0,0 +1,6 @@
+MODEL = {
+    'tiny': r'E:\CV\model\sam2.1_hiera_tiny.pt',
+    'small': r'E:\CV\model\sam2.1_hiera_small.pt',
+    'base': r'E:\CV\model\sam2.1_hiera_base_plus.pt',
+    'large': r'E:\CV\model\sam2.1_hiera_large.pt'
+}
